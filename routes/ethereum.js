fbdff4074df4d0ac306bc0e11e140776c704206c
jeano <kkangboss@gmail.com>
2020-12-21 15:41:20 +0900

    특정 블록넘버에 대한 블록조회기능

--- routes/ethereum.js
+++ routes/ethereum.js
@@ -12,13 +12,12 @@ let web3 = new Web3(
 
 router.get('/getBlock', function(req, res, next) {
     //현재 블록체인 에서 생성된 블록의 정보를 조회하여 보여주는 기능.
-    web3.eth.getBlock("latest", true, function(err,block)
+    var block_num = Number(req.query.block_num) | "latest";
+    web3.eth.getBlock(block_num, true, function(err,block)
     {
         console.log(block);
         res.json(block);
     })
-
-
 });
 
 module.exports = router;
