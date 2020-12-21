32d49b9258147fc5a2f50d026a7a3e1fef06f56d
jeano <kkangboss@gmail.com>
2020-12-21 15:13:04 +0900

    Ethereum  라우터 추가  - GetBlock API 구현

---
+++ routes/ethereum.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+require('dotenv').config();
+
+
+const Web3 = require('web3')
+let web3 = new Web3(
+    new Web3.providers.WebsocketProvider(process.env.INFURA_RPC_URL)
+)
+
+
+router.get('/getBlock', function(req, res, next) {
+    //현재 블록체인 에서 생성된 블록의 정보를 조회하여 보여주는 기능.
+    web3.eth.getBlock("latest", true, function(err,block)
+    {
+        console.log(block);
+        res.json(block);
+    })
+
+
+});
+
+module.exports = router;
