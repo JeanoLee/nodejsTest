5266be01e08022f33f855558f5b4a1fc3c496cd1
jeano <kkangboss@gmail.com>
2020-12-23 16:08:34 +0900

    컨트랙트 연결 및 데이터 조회 기능 테스트

--- routes/vote.js
+++ routes/vote.js
@@ -3,25 +3,47 @@ const { render } = require('pug');
 var router = express.Router();
 require('dotenv').config();
 
+let fs = require('fs');
+
+
+const Web3 = require('web3')
+let web3 = new Web3(
+    new Web3.providers.WebsocketProvider(process.env.INFURA_RPC_URL)
+)
+
 
 //localhost:3000/vote
 router.get("/", function(req,res,next){
+    
+    var voteFactoryAbi = JSON.parse( fs.readFileSync('abis/voteFactory.abi.json') )
+    var voteFactoryContract = new web3.eth.Contract(voteFactoryAbi, process.env.CONTRACT_ADDR_VOTE)
 
-    var data = {
-        factory : {
-            address :'0x0000',
-            count :3
-        },
-        votelist : [
-            { title:'테스트123'},
-            { title:'테스트234'},
-            { title:'테스트345'},
-            { title:'테스트456'},
-            { title:'테스트567'}
-        ]
-    }
+    voteFactoryContract.methods.countOfVote().call()
+    // call() : 상수함수들 view/pure, public으로 지정된 상태변수
+    // send() : 그 외의 함수들(상태변수를 변경하는 함수들) 
+    //        : 트랜잰션을 만들어서 전송함. ** Private Key가 필요
 
-    res.render('voteDashboard',{data:data});
+    .then( function(result){
+
+        var data = {
+            factory : {
+                address :process.env.CONTRACT_ADDR_VOTE,
+                count :result
+            },
+            votelist : [
+                { title:'테스트123'},
+                { title:'테스트234'},
+                { title:'테스트345'},
+                { title:'테스트456'},
+                { title:'테스트567'}
+            ]
+        }
+    
+        res.render('voteDashboard',{data:data});
+
+    })
+
+    
 })
 
 module.exports = router;
