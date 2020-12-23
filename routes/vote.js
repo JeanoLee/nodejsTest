f1fec7e3e55fd7a0499b66178e74f2fbd7af4b37
jeano <kkangboss@gmail.com>
2020-12-23 17:24:40 +0900

    투표 타이틀가져오는 기능 구현

--- routes/vote.js
+++ routes/vote.js
@@ -13,7 +13,7 @@ let web3 = new Web3(
 
 
 //localhost:3000/vote
-router.get("/", function(req,res,next){
+router.get("/", async function(req,res,next){
     
     var voteFactoryAbi = JSON.parse( fs.readFileSync('abis/voteFactory.abi.json') )
     var voteFactoryContract = new web3.eth.Contract(voteFactoryAbi, process.env.CONTRACT_ADDR_VOTE)
@@ -23,28 +23,51 @@ router.get("/", function(req,res,next){
     // send() : 그 외의 함수들(상태변수를 변경하는 함수들) 
     //        : 트랜잰션을 만들어서 전송함. ** Private Key가 필요
 
-    voteFactoryContract.methods.countOfVote().call()
-    .then( function(result){
+    // 네트워크를 통해서 데이터를 읽어오는 행위. -> IO 작업.
+    // IO 작업이 끝나면, Callback 함수를 호출하여, 남은 작업을 수행.
+    //
+    //
+    // 작업 1....
+    // voteFactoryContract.methods.countOfVote().call()
+    // .then(function (result){
+    //     //작업 2....
+    //     voteFactoryContract.methods.countOfVote().call()
+    //     .then( function (result2){
+    //         //작업 3....
+    //
+    //     })
+    // 
+    // })
 
-        var data = {
-            factory : {
-                address :process.env.CONTRACT_ADDR_VOTE,
-                count :result
-            },
-            votelist : [
-                { title:'테스트123'},
-                { title:'테스트234'},
-                { title:'테스트345'},
-                { title:'테스트456'},
-                { title:'테스트567'}
-            ]
-        }
-    
-        res.render('voteDashboard',{data:data});
 
-    })
+    let count = Number.parseInt(await voteFactoryContract.methods.countOfVote().call())
+    let voteAddressList = []
+    let titleList=[]
 
-    
+    for(let i=0;i<count;i++){
+        let _address = await voteFactoryContract.methods.findVoteByIndex(i).call()
+        voteAddressList.push(_address);
+    }
+    for(let i=0;i<count;i++){
+        let voteAddress = voteAddressList[i]
+        let voteAbi = JSON.parse( fs.readFileSync('abis/vote.abi.json'))
+        let voteContract = new web3.eth.Contract( voteAbi, voteAddress)
+        let title = await voteContract.methods.title().call()
+        titleList.push({
+            title : title
+        });
+    }
+
+       
+    var data = {
+        factory : {
+            address :process.env.CONTRACT_ADDR_VOTE,
+            count :count
+        },
+        votelist : titleList
+    }
+
+    res.render('voteDashboard',{data:data});
 })
 
 module.exports = router;
