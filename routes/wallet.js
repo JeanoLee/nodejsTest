6080508a1478025b4cc87c9fa0f67ab044e4df51
jeano <kkangboss@gmail.com>
2020-12-26 13:58:58 +0900

    token리스트 조회 추가

--- routes/wallet.js
+++ routes/wallet.js
@@ -10,11 +10,17 @@ const Web3 = require('web3')
 let web3 = new Web3(
     new Web3.providers.HttpProvider(process.env.INFURA_RPC_URL)
 )
+let tokenList;
+
+function loadTokenList(){
+    let data = fs.readFileSync('data/tokenList.json')
+    tokenList = JSON.parse(data);
+}
+loadTokenList();
 
 
 router.get("/", async function(req,res,next){
-    res.render('wallet',{})
-
+    res.render('wallet',{tokenList:tokenList})
 })
 
 
